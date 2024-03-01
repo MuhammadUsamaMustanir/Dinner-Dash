2ffbf73b7a0b627e05756a5a5180d3c8e4bf2de5
MuhammadUsamaMustanir <usamamustanir0@gmail.com>
2024-03-01 21:04:39 +0500

    make the user view

--- user/views.py
+++ user/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import User
+
+
+def get_all_users(request, first_name):
+    user = User.objects.all()
+    context = {"user": user}
+    render(request, 'templates/users.html', context)

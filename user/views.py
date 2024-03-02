3603c6b33be0fc49143dd270a83b1449ef75dfcd
MuhammadUsamaMustanir <usamamustanir0@gmail.com>
2024-03-02 22:21:54 +0500

    start the user templates

--- user/views.py
+++ user/views.py
@@ -6,4 +6,4 @@ from .models import User
 def get_all_users(request, first_name):
     user = User.objects.all()
     context = {"user": user}
-    render(request, 'templates/users.html', context)
+    render(request, 'user/templates/users.html', context)

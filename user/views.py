e7176c36504298fbb3147225a262e47d61c82946
MuhammadUsamaMustanir <usamamustanir0@gmail.com>
2024-03-04 23:31:07 +0500

    made the first http response endpoint

--- user/views.py
+++ user/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from .models import User
 
@@ -7,3 +8,7 @@ def get_all_users(request, first_name):
     user = User.objects.filter(first_name=first_name)
     context = {"user": user}
     return render(request, 'users.html', context)
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")

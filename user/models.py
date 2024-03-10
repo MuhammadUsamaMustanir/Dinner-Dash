a92e2d08e7526f7d08cf095137737796bfdd10d6
MuhammadUsamaMustanir <usamamustanir0@gmail.com>
2024-03-10 19:37:02 +0500

    use email as authentication

--- user/models.py
+++ user/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-class User(models.Model):
-    first_name = models.CharField(max_length=70)
-    last_name = models.CharField(max_length=70)
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
